f64683a9d8ae1489488515993fc1f0b85c1c0380
Scott Millar <scottmillar95@gmail.com>
2015-12-05 10:22:29 +0000

    PLEASE COMMIT

---
+++ Assets/Movement.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+		
+	public float speed = 4f;
+	public float jumpHeight;
+	public bool isJumping = false;
+	public bool wallJump = false;
+	public bool onWall = false;
+		
+		void start()
+	{
+	}
+
+		// Update is called once per frame
+		void Update () 
+		{
+			
+			if (Input.GetKey (KeyCode.D)) {
+				transform.Translate(Vector2.right * speed * Time.deltaTime);    
+			}
+			else if (Input.GetKey (KeyCode.A)) {
+				transform.Translate(-Vector2.right * speed * Time.deltaTime);   
+			}
+			else if (Input.GetKey (KeyCode.S)) {
+				transform.Translate(-Vector2.up * speed * Time.deltaTime);  
+			}
+			if (Input.GetKeyDown (KeyCode.Space)) 
+			{
+				if (isJumping == false)
+				{
+					GetComponent<Rigidbody2D> ().velocity = Vector2.up * jumpHeight;
+
+					isJumping = true;
+
+				}
+				else if(onWall == true && wallJump == false)
+				{
+					GetComponent<Rigidbody2D> ().velocity = Vector2.up * jumpHeight;
+					wallJump = true;
+				}
+			}
+		}
+		
+
+
+		void OnCollisionEnter2D(Collision2D col)
+	{
+		if (col.gameObject.tag == "Ground") 
+		{
+			isJumping = false;
+			wallJump = false;
+		}
+
+		if (col.gameObject.tag == "Wall") 
+		{
+			onWall = true;
+		}
+	}
+
+		void OnCollisionExit2D(Collision2D col)
+	{
+		if (col.gameObject.tag == "Wall") 
+		{
+			onWall = false;
+		}
+	}
+}
---
+++ Assets/Shoot.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour {
+
+	private Vector3 mouse_pos;
+	private Vector3 object_pos;
+	private float angle;
+	private float bulletSpeed = 20;
+	public GameObject bulletPrefab;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate () {
+		if (Input.GetMouseButtonDown (0))
+		{
+			mouse_pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			Vector2 dir = new Vector2(mouse_pos.x - transform.position.x, mouse_pos.y - transform.position.y);
+			dir.Normalize();
+
+			GameObject bullet = (GameObject)Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+			bullet.transform.eulerAngles = new Vector3(0, 0, Mathf.Atan2((mouse_pos.y - transform.position.y), (mouse_pos.x - transform.position.x)) * Mathf.Rad2Deg);
+			bullet.GetComponent<Rigidbody2D>().velocity = dir * bulletSpeed;
+		}
+	}
+}
+
+
